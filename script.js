c0ef1bfb88f5a9ea8838a633c098b32ccfed7bd2
faridbpn <farid.IDNSOLO@gmail.com>
2025-01-07 14:30:58 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// document.querySelectorAll('.filter-btn').forEach(button => {
+//     button.addEventListener('click', () => {
+//       const filter = button.getAttribute('data-filter');
+//       document.querySelectorAll('.menu-item').forEach(item => {
+//         item.style.display = filter === 'all' || item.getAttribute('data-category') === filter ? 'block' : 'none';
+//       });
+//     });
+//   });
+  
